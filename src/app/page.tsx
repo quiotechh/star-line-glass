675d2d51623f7df4b3e8b8c99f025340dd02eec0
quiotechh <quiotechh@gmail.com>
2026-01-31 22:42:37 +0530

    Added products overview page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/AboutSection";
 import CredibilitySection from "@/components/CredibilitySection";
 import HeroSection from "@/components/HeroSection";
+import ProductsOverview from "@/components/ProductsOverview";
 
 
 
@@ -9,6 +10,7 @@ export default function HomePage() {
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <HeroSection />
     <CredibilitySection />
+    <ProductsOverview />
     <AboutSection />
     </div>
   );
